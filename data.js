2a6204e9cedf6ee29230b58892d3be8ece3dfcc7
Jonathan <jonatjano@gmail.com>
2019-10-29 21:48:51 +0100

    created router (#1)

---
+++ data.js
@@ -0,0 +1,14 @@
+const wikiData = {
+    monster: [
+        {
+            name: "lizard",
+            img: "path/to/lizard.png",
+            description: "lizard's desc"
+        },
+        {
+            name: "slime",
+            img: "path/to/slime.png",
+            description: "slime's desc"
+        }
+    ]
+}
---
+++ elements/monster.js
@@ -0,0 +1,31 @@
+customElements.define(
+    "hb-wiki-monster",
+    class extends HTMLElement {
+        constructor() {
+            super();
+            this.attachShadow({mode: 'open'})
+            this.body = document.createElement("div")
+            this.body.id = "body"
+            this.shadowRoot.appendChild(this.body)
+        }
+
+        updateContent() {
+            this.body.textContent = this.path
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+            if (name === "path") {
+                if(newValue !== oldValue) {
+                    this.updateContent()
+                }
+            }
+        }
+
+        static get observedAttributes() {
+            return ["path"]
+        }
+
+        set path(val) { this.setAttribute("path", val); return this }
+        get path() { return this.getAttribute("path") }
+    }
+);
